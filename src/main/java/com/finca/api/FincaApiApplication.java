082790a8dbae84f0e9412c9042f2ecc27fa7ff9a
JosuePaiva02 <u202119095@upc.edu.pe>
2026-03-07 14:21:57 -0500

    feat: set up work environment

---
+++ src/main/java/com/finca/api/FincaApiApplication.java
@@ -0,0 +1,13 @@
+package com.finca.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finca/api/FincaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finca.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
