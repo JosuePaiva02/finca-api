2e238911e989a4a8692c41a9c815ca6953163887
JosuePaiva02 <u202119095@upc.edu.pe>
2026-03-16 19:40:43 -0500

    feat: implemented queries service

--- src/main/java/com/finca/api/properties/domain/services/PropertiesQueriesService.java
+++ src/main/java/com/finca/api/properties/domain/services/PropertiesQueriesService.java
@@ -1,4 +1,13 @@
 package com.finca.api.properties.domain.services;
 
+import com.finca.api.properties.domain.model.aggregates.Property;
+import com.finca.api.properties.domain.model.queries.*;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PropertiesQueriesService {
+        List<Property> handle(GetFeaturedPropertiesQuery query);
+        Optional<Property> handle(GetPropertyByIdQuery query);
+        List<Property> handle(SearchedPropertiesQuery query);
 }
--- src/main/java/com/finca/api/properties/infrastructure/persistence/jpa/repositories/PropertiesRepository.java
+++ src/main/java/com/finca/api/properties/infrastructure/persistence/jpa/repositories/PropertiesRepository.java
@@ -2,8 +2,9 @@ package com.finca.api.properties.infrastructure.persistence.jpa.repositories;
 
 import com.finca.api.properties.domain.model.aggregates.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface PropertiesRepository extends JpaRepository<Property, Long> {
 
 }
